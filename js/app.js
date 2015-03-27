42d416262774cfee6021899e451004aa2c562e40
Frédéric Matigot <fred@fspot.org>
2015-03-27 12:09:43 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,15 +9,17 @@ app.value("defaultRadios", [
 	{url: "http://streaming.radio.rtl.fr/rtl2-1-44-64.mp3", name: "RTL2", isPlaying: false},
 	{url: "http://mp3.live.tv-radio.com/francemusique/all/francemusiquehautdebit.mp3", name: "France Musique", isPlaying: false},
 	{url: "http://adwzg3.tdf-cdn.com/8470/nrj_165631.mp3", name: "NRJ", isPlaying: false},
-	{url: "http://95.81.146.2/collinesfm/all/collines.mp3", name: "Collines", isPlaying: false},
+	{url: "http://95.81.155.3/collinesfm/all/collines.mp3", name: "Collines", isPlaying: false},
 	{url: "http://diffusion.lafrap.fr/alternantes.mp3", name: "Alternantes", isPlaying: false},
-	{url: "http://vipicecast.yacast.net/virginradio_192", name: "Virgin Radio", isPlaying: false},
+	{url: "http://mp3lg4.tdf-cdn.com/9243/lag_164753.mp3", name: "Virgin Radio", isPlaying: false},
 	{url: "http://live.francra.org:8000/radiocanut", name: "Radio Canut", isPlaying: false},
 	{url: "http://ecoutez.radio-libertaire.org:8080/radiolib", name: "Radio Libertaire", isPlaying: false},
 	{url: "http://radio.toile-libre.org:8000/fpp.mp3", name: "FPP", isPlaying: false},
 	{url: "http://swingfm.ice.infomaniak.ch:80/swingfm-128", name: "Swing FM", isPlaying: false},
 	{url: "http://broadcast.infomaniak.net/radionova-high.mp3", name: "Nova", isPlaying: false},
-	{url: "http://radio.rim952.fr:8000/stream.mp3", name: "Ici et Maintenant", isPlaying: false}
+	{url: "http://radio.rim952.fr:8000/stream.mp3", name: "Ici et Maintenant", isPlaying: false},
+	{url: "http://91.121.65.189:8000/2", name: "Radio Zinzine", isPlaying: false},
+	{url: "http://s8.voscast.com:7742/;stream1427453898011/1", name: "Vosstanie", isPlaying: false},
 ]);
 
 app.config(function($routeProvider) {
